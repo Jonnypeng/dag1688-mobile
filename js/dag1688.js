1edfd55dc06eade56b6b769fe7a3d7e5711f0259
jonnypeng <jonny.peng@qq.com>
2016-05-25 11:27:13 +0800

    增加了二级菜单的激活样式

--- js/dag1688.js
+++ js/dag1688.js
@@ -3,6 +3,9 @@ var hiddenPc = document.getElementsByTagName("meta")[2];
 var pageContent = document.getElementsByClassName("entry-content")[0];
 var pageImg = pageContent.getElementsByTagName("img");
 var header = document.getElementById("header");
+var nav = document.getElementById("menu-%e4%b8%bb%e7%9b%ae%e5%bd%95");
+var navLi = nav.getElementsByTagName("li");
+
 
 if(browserWidth < 1000){
 				hiddenPc.content = "width=device-width, initial-scale=1,user-scalable = no"
@@ -19,9 +22,17 @@ if(browserWidth > 1000){
 }else{
 			header.className = "";
 };	
-								
 };
 }
+
+for(var i in navLi){
+	navLi[i].onmouseover = function (){
+		this.getElementsByTagName("ul")[0].style.display = "block";
+};
+	navLi[i].onmouseout = function (){
+		this.getElementsByTagName("ul")[0].style.display = "none";
+};
+};
 
 
 
